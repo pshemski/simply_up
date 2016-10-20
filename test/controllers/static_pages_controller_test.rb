d75c8eeb3c4f0fceb517554a89a830de97c85a58
Przemek Markowski <p.markowski1980@gmail.com>
2016-10-20 16:38:42 +0700

    finished static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,21 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About Us | Ruby on Rails Tutorial"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial"
   end
 
 end
