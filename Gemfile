559a50e646cb2a0911a1312a6b12f1b35a8daaec
Przemek Markowski <p.markowski1980@gmail.com>
2016-10-24 18:03:05 +0800

    starting test again

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'jquery-rails', '4.0.3'
 gem 'turbolinks', '2.3.0'
 gem 'jbuilder', '~> 2.2.3'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'rb-readline'
 
 # gem 'bcrypt', '~> 3.1.7'
 # gem 'unicorn'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,28 +1,33 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @title_base = "Ruby on Rails Tutorial"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial"
+    assert_select "title", "Home | #{@title_base}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial"
+    assert_select "title", "Help | #{@title_base}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About Us | Ruby on Rails Tutorial"
+    assert_select "title", "About Us | #{@title_base}"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial"
+    assert_select "title", "Contact | #{@title_base}"
   end
 
 end
