25076815c5e473b780a8f09a05fab55ac4a75c1b
Przemek Markowski <p.markowski1980@gmail.com>
2016-10-24 16:40:08 +0700

    modified test helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def full_title(page_title = '')
+		title_base = "Ruby on Rails Tutorial"
+		if page_title.empty?
+			title_base
+		else
+			"#{page_title} | #{title_base}"
+		end
+	end
 end
